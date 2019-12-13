de1611f3d63a86aa394af68b1a56fbb6d498836a
Nizal <alshammry-nazal@hotmail.com>
2019-12-13 16:06:17 +0000

    Create project.clj

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject tawny-chebi "1.0.0"
+  :description "A mavenized version of ChEBI"
+  :dependencies [[uk.org.russet/tawny-owl "1.3.0"]]
+  :scm {:url "https://github.com/jaydchan/tawny-chebi.git"
+        :name "git"}
+  :license {:name "LGPL"
+            :url "http://www.gnu.org/licenses/lgpl-3.0.txt"
+            :distribution :repo}
+  :jvm-opts ["-Xmx1000m"]
+  ;; :main tawny-chebi.core
+  )
